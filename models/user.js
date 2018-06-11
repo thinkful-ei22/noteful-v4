a748617e64e7a8381722fd3a9259044ee9faff22
Chris Klanac <cklanac@users.noreply.github.com>
2018-06-11 16:23:25 -0400

    s1-local-auth

---
+++ models/user.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
+
+// ===== Define UserSchema & UserModel =====
+const userSchema = new mongoose.Schema({
+  fullname: { type: String, default: '' },
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+});
+
+// Customize output for `res.json(data)`, `console.log(data)` etc.
+userSchema.set('toObject', {
+  virtuals: true,     // include built-in virtual `id`
+  versionKey: false,  // remove `__v` version key
+  transform: (doc, ret) => {
+    delete ret._id; // delete `_id`
+    delete ret.password;
+  }
+});
+
+userSchema.methods.validatePassword = function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+userSchema.statics.hashPassword = function (password) {
+  return bcrypt.hash(password, 10);
+};
+
+module.exports = mongoose.model('User', userSchema);
