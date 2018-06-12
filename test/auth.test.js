a9b15b0a08e138599ea29fc4dccf6e0197d20b63
Chris Klanac <cklanac@users.noreply.github.com>
2018-06-12 18:38:37 -0400

    chore: updated dependencies

--- test/auth.test.js
+++ test/auth.test.js
@@ -13,7 +13,7 @@ const User = require('../models/user');
 const expect = chai.expect;
 chai.use(chaiHttp);
 
-describe.only('Noteful API - Login', function () {
+describe('Noteful API - Login', function () {
 
   let token;
   const _id = '333333333333333333333333';
